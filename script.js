89373612c9b70703eb447e2e403dd1113b5b9209
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2020-12-13 10:45:15 +1100

    color working need to fix input

--- script.js
+++ script.js
@@ -5,25 +5,36 @@ $( document ).ready(function() {
  let m =  moment();
  let output = m.format("dddd MMM Mo YYYY");
 
+ console.log(output) 
  $("#currentDay").text(output)
         
- console.log(output)    
+   
 
-
- var currentHour = Date.now().getHours();
+let newTime = new Date();
+ var currentHour = newTime.getHours();
  console.log(currentHour)
- $('.colorcode').each(function(){
+ $('.edit_cont').each(function(){
     var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour+6){
-        $(this).css('background-color','grey');
-    }else if(val < currentHour && val > currentHour-6){
-        $(this).css('background-color','green');
+    if(val > currentHour && val < currentHour+8){
+        $(this).css('background-color','lightgreen');
+    }else if(val < currentHour && val > currentHour-8){
+        $(this).css('background-color','lightgrey');
     }else if(val === currentHour){
-        $(this).css('background-color','red');
+        $(this).css('background-color','salmon');
     }else{
         $(this).css('background-color','white');
     }
- });
+});
+//add input under edit_cont class 
+let newPlan = $("<input>")
+newPlan.addClass("inputPlan");
+newPlan.text($(".edit_cont"));
+$(".edit_cont").append(newPlan);
+
+ 
+
+//addEventlister under edit_btn div
+
 
 
 // localStorage.setItem() to save items
