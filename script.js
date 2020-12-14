d8701f2cbae2cc96cb65f9d8a2046f755649fadf
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2020-12-14 13:55:52 +1100

    everhting working

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ $( document ).ready(function() {
    
 
 let newTime = new Date();
- var currentHour = newTime.getHours();
+ let currentHour = newTime.getHours();
  console.log(currentHour)
  $('.edit_cont').each(function(){
-    var val = parseInt($(this).prop('id'));
+    let val = parseInt($(this).prop('id'));
     if(val > currentHour && val < currentHour+9){
         $(this).css('background-color','lightgreen');
     }else if(val < currentHour && val > currentHour-9){
@@ -26,35 +26,70 @@ let newTime = new Date();
     }
 });
 //make a textarea with a special class hour${i}, put it in the right div
-    //add input under edit_cont class 
+//add input under edit_cont class 
+
+function createTimeRow(time) {
+    let row = $("<div>");
+    row.addClass('row myrow');
+    let timeCol = $("<div>");
+    timeCol.addClass("col-1 my-col col-a1 colorcode");
+    let timeMeridian;
+    if(time < 12){
+        timeMeridian = 'am'
+    }else{
+        time = time === 12 ? time : time - 12;
+        timeMeridian = 'pm'
+    }
+    timeCol.text(time + timeMeridian);
+    
+    let textareaCol = $("<div>");
+    textareaCol.addClass('col-10 my-col col-a2 edit_cont textarea9');
+    textareaCol.attr('id', time);
+    
+    
+    let buttonCol = $("<div>");
+    buttonCol.addClass("col-1 my-col col-a3 save_btn");
+    buttonCol.attr('id', 'saveBtn' + time)
+
+    row.append(timeCol, textareaCol, buttonCol);
+
+    return row;
+}
+let container = $('.container');
    
-for(var i = 9; i<18; i++){
+for(let i = 9; i<18; i++){
+
+    //let row = createTimeRow(i);
+    //container.append(row);
+
+
+    let existingPlan = window.localStorage.getItem(i) || "";
     let newPlan = $(`<textarea>`);
+    newPlan.val(existingPlan);
     newPlan.addClass(`hours${i}`);
+
     $(".textarea" + i).append(newPlan);
 
      //make a saveBtn, put it in the right div. Have it grab info from class hour${i}
-   let saveBtn = $("<button>");
-   saveBtn.addClass(`hours${i}`);
-   saveBtn.html("Save");
+    let saveBtn = $("<button>");
+    saveBtn.addClass(`hours${i}`);
+    saveBtn.html("Save");
 
-saveBtn.click(function() {
-    console.log($(this).parent().siblings().children());
+saveBtn.click(function(event) {
+    //.log($(this).parent().siblings().children());
+    //console.log($(this).parent().parent().children()[0].textContent);
     let planText = $(this).parent().siblings().children().val();
-    console.log(planText);
+    //console.log(planText);
+    //console.log(event.target.id)
+    //console.log($(this).closest(".save_btn").attr("id").slice(7));
   
-    localStorage.setItem("plan", JSON.stringify(planText));   
+    localStorage.setItem(i, planText);      
    
   });
 
 $("#saveBtn" + i).append(saveBtn);
 };
 
-
-
-
-// let plan = newPlan.value;
-// localStorage.setItem("plan", JSON.stringify(plan));
 
 // localStorage.setItem() to save items
 // It takes two arguments, a key, and a value
